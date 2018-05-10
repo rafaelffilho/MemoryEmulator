92f754fe41b2be6bba5f066282083aba7983a040
Adolpho Piazza <adolphopiazza@gmail.com>
2018-05-09 21:06:56 -0300

    Front-end updates

--- process.js
+++ process.js
@@ -5,8 +5,8 @@ var listProcesses = [];
 var listWait = [];
 //Create memory
 var memory;
-memorySize = "2048";
-pageSize = "64";
+memorySize = "1024";
+pageSize = "128";
 time = 0;
 
 class Process {
@@ -31,10 +31,19 @@ class Memory {
 function doTick() {
   var i = 0;
   time++;
+  //Update time label
+  var timeLabel = document.getElementById("timeLabel").innerHTML = "Tempo: " + time;
+  //Updating the memory div
+  var memoryLabel = document.getElementById("memoryLabel").innerHTML = "Tamanho da Memória: " + memorySize;
+  var sizeLabel = document.getElementById("sizeLabel").innerHTML = "Tamanho da Página: " + pageSize;
+  var memoryTable = document.getElementById("memoryTable");
+  var memoryWaitTable = document.getElementById("memoryWaitTable");
+
   console.log("Memory", memory);
   console.log("Processes list", listProcesses);
   console.log("Wait list", listWait);
   console.log("Time", time);
+
   if (memory.pageList != null) {
     for (var j = 0; j < memory.pageList.length; j++) {
       if (memory.pageList[j] != null) {
@@ -81,6 +90,26 @@ function doTick() {
       listWait.push(listProcesses[i]);
       listProcesses.splice(i, 1);
       break;
+    }
+  }
+
+  //Clear the table
+  $("#memoryTable td").remove();
+  //Update the memory table
+  for (var i = 0; i < memory.pageList.length; i++) {
+    if (memory.pageList[i] != null) {
+      var row = memoryTable.insertRow(1);
+      var processInMemory = row.insertCell(0).innerHTML = "Processo: " + memory.pageList[i].pName + ". Tamanho do Processo: " + memory.pageList[i].pSize + "kb";
+    }
+  }
+
+  //Clear table
+  $("#memoryWaitTable td").remove();
+  //Update the wait process list table
+  for (var i = 0; i < listWait.length; i++) {
+    if (listWait[i] != null) {
+      var row = memoryWaitTable.insertRow(1);
+      var processInList = row.insertCell(0).innerHTML = "Processo: " + listWait[i].pName + ". Tamanho do Processo: " + listWait[i].pSize + "kb";
     }
   }
 }
